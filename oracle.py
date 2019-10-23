227ba7ef231c3dd4163d0e321b22de5f934282da
Jessica <jessica.keune@ugent.be>
2019-10-23 18:00:12 +0200

    adding this famous oracle (thanks dominik)

---
+++ oracle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+while True:
+
+    number = input("An oracle will provide a YES or NO answer to any question if you type a 4 digit (unique) integer:\n")
+    
+    try: 
+        int(number)
+        
+        if len(number)==4:
+            _ = input("Now type your question:\n")
+            if (int(number[0]) + int(number[1]) + int(number[2]) + int(number[3]))%2==0:
+                print("---> YES")
+                break
+            else:
+                print("---> NO")
+        else:
+            print("---> That's not a 4 digit number..")
+        
+    except ValueError:
+        print("---> That's not a number..")
